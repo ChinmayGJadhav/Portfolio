996ff6e90b29eaa01b04c825f77e0a21da220a4d
ChinmayGJadhav <51798073+ChinmayGJadhav@users.noreply.github.com>
2020-06-11 00:53:40 +0530

    Add files via upload

---
+++ newIndex.js
@@ -0,0 +1,14 @@
+AOS.init({
+  offset:120,
+  duration:1000,
+});
+
+
+
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+//rellax
+let rellax= new Rellax('.rellax')
